0b8b65154cb5362439d4053f158b0f8ea78adabd
InDev <InDevRus@hotmail.com>
2023-03-21 02:37:50 +0500

    Solved

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,24 @@
 package com.epam.mjc.stage0;
 
-public class Animal {
+import java.text.MessageFormat;
 
+public class Animal {
+    private final String color;
+    private final int numberOfPaws;
+    private final boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        return MessageFormat.format("This animal is mostly {0}. It has {1} {3} and {2} fur.",
+                color,
+                Integer.toString(numberOfPaws),
+                hasFur ? "a" : "no",
+                numberOfPaws > 1 ? "paws" : "paw"
+        );
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,14 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+import java.text.MessageFormat;
 
+public class Bird extends Animal {
+    public Bird() {
+        super("blue", 2, false);
+    }
+
+    @Override
+    public String getDescription() {
+        return MessageFormat.format("{0} Moreover, it has 2 wings and can fly.", super.getDescription());
+    }
 }
